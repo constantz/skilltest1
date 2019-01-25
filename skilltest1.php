6867499531f3444fb40bc6c03fe08453502a96e2
Constant <c.zwijnenberg@gmail.com>
2019-01-25 15:35:48 +0100

    sorting function added

--- skilltest1.php
+++ skilltest1.php
@@ -2,28 +2,41 @@
     // lees file input.txt
     $invoer = file('input.txt');
 
-    //var_dump($invoer);
+    var_dump($invoer);
 
     // zit nu in array index => 'naam; score'
     // zet in associative array [naam, score]
     
-    $i = 0;
-
     $lijst = [];
     foreach($invoer as $inv){
         $aregel = explode(';', $inv);
-        
-            $naam =  $aregel[0];
-            $score = (int)($aregel[1]);
-            $lijst[]  = ['naam' => $naam, 'score' => $score   ];
-        $i++;
-
+        $naam =  $aregel[0];
+        $score = (int)($aregel[1]);
+        $lijst[]  = ['naam' => $naam, 'score' => $score   ];
     }
-    
+    var_dump($aregel);
     var_dump($lijst);
 
+    function cmp($rija, $rijb) {
+        if ($rija['score'] == $rijb['score']) {
+            return 0;
+        }
+        return ($rija['score'] < $rijb['score']) ? -1 : 1;
+    }
 
+    uasort($lijst, 'cmp');
     // sorteer op score
+    // $sorting = true;
+
+    // while $sorting {
+    //     $sorting = false;
+    //     foreach($lijst as $element){
+    //         if($element['score'])
+    //     }
+
+
+    // }
+
 
 
     // schrijf naar output.txt
