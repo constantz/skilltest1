4344a2e0f22cb18ead90c31cf845a8f6020a75d3
Constant <c.zwijnenberg@gmail.com>
2019-01-25 16:48:46 +0100

    sort function refactored, comments removed, now super small program

--- skilltest1.php
+++ skilltest1.php
@@ -1,12 +1,5 @@
 <?php
-    // lees file input.txt
     $invoer = file('input.txt');
-
-    var_dump($invoer);
-
-    // zit nu in array index => 'naam; score'
-    // zet in associative array [naam, score]
-    
     $lijst = [];
     foreach($invoer as $inv){
         $aregel = explode(';', $inv);
@@ -14,32 +7,10 @@
         $score = (int)($aregel[1]);
         $lijst[]  = ['naam' => $naam, 'score' => $score   ];
     }
-    var_dump($aregel);
-    var_dump($lijst);
-
-    function cmp($rija, $rijb) {
-        if ($rija['score'] == $rijb['score']) {
-            return 0;
+    uasort($lijst, function ($rija, $rijb) {
+        return ($rija['score'] - $rijb['score']);
         }
-        return ($rija['score'] < $rijb['score']) ? -1 : 1;
-    }
-
-    uasort($lijst, 'cmp');
-    // sorteer op score
-    // $sorting = true;
-
-    // while $sorting {
-    //     $sorting = false;
-    //     foreach($lijst as $element){
-    //         if($element['score'])
-    //     }
-
-
-    // }
-
-
-
-    // schrijf naar output.txt
+    );
     $uitvoerString = "";
     foreach($lijst as $regel){
         $uitvoerString = $uitvoerString . $regel['naam'] . '; ' . $regel['score'] . "\n";
